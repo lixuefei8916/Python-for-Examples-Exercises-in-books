fa9ec39ca7b8a14485397f243f3878bdd7bbf62e
lixuefei8916 <lixuefei8916@126.com>
2015-12-02 18:12:31 +0800

    login登录模板(py+html)
    login登录模板(py+html)

---
+++ Flask/[flask] login登录模板/view.py
@@ -0,0 +1,31 @@
+#-*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',debug=True)
+
