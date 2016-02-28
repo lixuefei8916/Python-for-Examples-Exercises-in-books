0796e5b453c5f56df29d65e7adb50897fe06a701
lixuefei8916 <lixuefei8916@126.com>
2016-02-28 18:24:34 +0800

    元组和序列
    元组和序列

--- Tutorial/5. Data Structures[数据结构]/5.3. Tuples and Sequences.py
+++ Tutorial/5. Data Structures[数据结构]/5.3. Tuples and Sequences.py
@@ -10,3 +10,14 @@
 u = t, (1, 2, 3, 4, 5)
 >>> u
 ((12345, 54321, 'hello!'), (1, 2, 3, 4, 5))
+
+
+>>> # Tuples are immutable:
+... t[0] = 88888
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'tuple' object does not support item assignment
+
+
+
+
