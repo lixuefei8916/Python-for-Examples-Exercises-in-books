fe8834a091a1f231fe94df10992ac81eb034da48
lixuefei8916 <lixuefei8916@126.com>
2016-02-29 20:21:46 +0800

    元组和序列
    元组和序列

--- Tutorial/5. Data Structures[数据结构]/5.3. Tuples and Sequences.py
+++ Tutorial/5. Data Structures[数据结构]/5.3. Tuples and Sequences.py
@@ -17,7 +17,10 @@ u = t, (1, 2, 3, 4, 5)
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 TypeError: 'tuple' object does not support item assignment
-
+>>> # but they can contain mutable objects:
+... v = ([1, 2, 3], [3, 2, 1])
+>>> v
+([1, 2, 3], [3, 2, 1])
 
 
 
